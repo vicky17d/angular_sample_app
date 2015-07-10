7745ca43a4e6ce857d32919045bded9e253d55ad
vicky17d <vicky17d@gmail.com>
2015-07-10 14:07:09 -0700

    finished the app, just tests left

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mytodoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mytodoApp
+ */
+angular.module('mytodoApp')
+  .controller('MainCtrl', function ($scope, localStorageService) {
+    var todosInStore = localStorageService.get('todos');
+
+    $scope.todos = todosInStore || [];
+
+    $scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);
+
+
+    $scope.addTodo = function() {
+    if ($scope.todo != '') {
+      $scope.todos.push($scope.todo);
+      $scope.todo = '';
+      }
+    };
+
+      $scope.removeTodo = function(index) {
+
+          $scope.todos.splice(index, 1);
+
+        };
+  });
